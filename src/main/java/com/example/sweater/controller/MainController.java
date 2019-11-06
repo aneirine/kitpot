6573e0ff637ba37c04eedb0cf5e7e7e70d82e980
Annchouss <anyashi2002@gmail.com>
2019-11-06 20:57:43 +0200

    Methods in MainController were chenged

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -3,24 +3,21 @@ package com.example.sweater.controller;
 import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import com.example.sweater.repository.MessageRepository;
-import com.example.sweater.utils.ControllerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
+import java.awt.print.Pageable;
 import java.util.Map;
-import java.util.UUID;
 
 
 @Controller
@@ -49,18 +46,21 @@ public class MainController {
     @GetMapping("main")
     public String main(@AuthenticationPrincipal User user,
                        @RequestParam(required = false) String filter,
-                       Model model) {
+                       Model model,
+                       @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
 
         if (user == null) {
             return "redirect:/login";
         } else {
-            Iterable<Message> messages;
-            if (filter != null && !filter.isEmpty()) {
+            Page<Message> page;
+            boolean condition = filter != null && !filter.isEmpty();
+            page = condition ? messageRepository.findByTag(filter, pageable) : getMessages(pageable);
+            /*if () {
                 messages = messageRepository.findByTag(filter);
             } else {
                 messages = getMessages();
-            }
-            model.addAttribute("messages", messages);
+            }*/
+            model.addAttribute("messages", page);
             model.addAttribute("filter", filter);
             return "main";
         }
@@ -68,71 +68,43 @@ public class MainController {
     }
 
 
-    // @PostMapping("add")
-   /* public String add(@AuthenticationPrincipal User user,
-                      @Valid Message message,
-                      BindingResult bindingResult,
-                      Model model,
-                      @RequestParam("file") MultipartFile file) throws IOException {
-        message.setAuthor(user);
-
-        if (bindingResult.hasErrors()) {
-            Map<String, String> collect = ControllerUtils.getErrors(bindingResult);
-            model.mergeAttributes(collect);
-            model.addAttribute("message", message);
-        } else {
-            if (file != null && !file.getOriginalFilename().isEmpty()) {
-                File uploadDir = new File(uploadPath);
-
-                if (!uploadDir.exists()) uploadDir.mkdir();
-
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-                file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-                message.setFilename(resultFilename);
-            }
-
-            model.addAttribute("message", null);
-            messageRepository.save(message);
-        }
-        model.addAttribute("messages", getMessages());
-        return "add";
-    }*/
-
-
     @PostMapping("deleteAll")
-    public String deleteAll(Map<String, Object> model) {
-        messageRepository.deleteAll(getMessages());
-        model.put("messages", getMessages());
+    public String deleteAll(Map<String, Object> model,
+                            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        messageRepository.deleteAll(getMessages(pageable));
+        model.put("messages", getMessages(pageable));
         return "redirect:/main";
     }
 
 
     @GetMapping("deleteById/{id}")
-    public String delete(@PathVariable Long id, Map<String, Object> model) {
+    public String delete(@PathVariable Long id,
+                         Map<String, Object> model,
+                         @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
         messageRepository.deleteById(id);
-        model.put("messages", getMessages());
+        model.put("messages", getMessages(pageable));
         return "redirect:/main";
     }
 
     @PostMapping("showAllMyMessages")
-    public String showAllMyMessages(@AuthenticationPrincipal User user, Map<String, Object> model) {
-        Iterable<Message> messages = messageRepository.findByAuthor(user);
+    public String showAllMyMessages(@AuthenticationPrincipal User user,
+                                    Map<String, Object> model,
+                                    @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        Iterable<Message> messages = messageRepository.findByAuthor(user, pageable);
         model.put("messages", messages);
         return "main";
     }
 
     @PostMapping("showAll")
-    public String showAll(Map<String, Object> model) {
-        model.put("messages", getMessages());
+    public String showAll(Map<String, Object> model,
+                          @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        model.put("messages", getMessages(pageable));
         return "redirect:/main";
     }
 
 
-    private Iterable<Message> getMessages() {
-        return messageRepository.findAll();
+    private Page<Message> getMessages(Pageable pageable) {
+        return messageRepository.findAll(pageable);
     }
 
 
--- src/main/java/com/example/sweater/repository/MessageRepository.java
+++ src/main/java/com/example/sweater/repository/MessageRepository.java
@@ -2,11 +2,14 @@ package com.example.sweater.repository;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);
-    List<Message> findByAuthor(User author);
+    Page<Message> findAll(Pageable pageable);
+    Page<Message> findByTag(String tag, Pageable pageable);
+    Page<Message> findByAuthor(User author, Pageable pageable);
 }
